04032f6dd29bf44d3a2b5aaa3135a2b0a2fad083
Chris Snodgrass <deans3@utas.edu.au>
2023-05-08 12:25:46 +1000

    Add project files.

---
+++ GROUP_07_RAP/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security;
+// uncomment when somehting added
+using KIT206_RAP.DataBase;
+using KIT206_RAP.Controll;
+using KIT206_RAP.Researchers;
+using KIT206_RAP.View;
+//using KIT206_RAP.DataBase;
+
+namespace GROUP_07_RAP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //ResearcherControl.DisplayResearchers();
+        }
+    }
+}
